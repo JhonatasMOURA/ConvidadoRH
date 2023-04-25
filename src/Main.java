e1933bce3147d1192b1e6dec630621fffd62f9ae
JhonatasMOURA <jhonatasalvesdemoura@gmail.com>
2023-04-25 17:16:32 -0300

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<ConvidadoRH> lista = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quantos convidados na festa?");
+        int quant = scanner.nextInt();
+        String numero = scanner.nextLine();
+        for (int i = 1; i <= quant; i++) {
 
-        ConvidadoRH convidadoRH = new ConvidadoRH("Jhonatas","123456789-12","11984477602","02-08-2006","02-08-2023","manhã");
-        System.out.println(convidadoRH.toString());
+            System.out.println("Qual o seu nome?");
+            String nome = scanner.nextLine();
+
+            System.out.println("Qual o seu cpf?");
+            String cpf = scanner.nextLine();
+
+            System.out.println("Qual o seu telefone?");
+            String telefone = scanner.nextLine();
+
+            System.out.println("Qual a sua data de nascimento?");
+            String dataNascimento = scanner.nextLine();
+
+            System.out.println("Qual a sua data de agendamento?");
+            String dataAgendamento = scanner.nextLine();
+
+            System.out.println("Qual o periodo?");
+            String periodo = scanner.nextLine();
+
+
+            ConvidadoRH convidados = new ConvidadoRH(nome, cpf, telefone, dataNascimento, dataAgendamento, periodo);
+            lista.add(convidados);
+
+        }
+        System.out.println(lista);
+
+
+
+
     }
 }
---
+++ src/Teste.java
@@ -0,0 +1,8 @@
+public class Teste {
+    public static void main(String[] args) {
+        ConvidadoRH convidadoRH = new ConvidadoRH("Jhonatas","123456789-12","11984477602","02-08-2006","05-01-2024","manhã");
+        System.out.println(convidadoRH.toString());
+    }
+
+
+}
